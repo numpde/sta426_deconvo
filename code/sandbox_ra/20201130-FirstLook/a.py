1a6f8d61c7b96db82e6d5353f92b750dc97e5840
RA <numpde@null.net>
2020-12-20 17:39:21 +0100

    exploratory figures

--- code/sandbox_ra/20201130-FirstLook/a.py
+++ code/sandbox_ra/20201130-FirstLook/a.py
@@ -1,8 +1,7 @@
 # RA, 2020-11-30
 
 """
-Preliminary exploratory analysis
-of the FGCZ dataset.
+Exploratory analysis of the FGCZ dataset.
 
 RA, 2020-11-30
 """
@@ -14,6 +13,24 @@ import numpy as np
 from pathlib import Path
 from plox import Plox
 from collections import OrderedDict
+from tcga.utils import mkdir, from_iterable
+
+rs = np.random.RandomState(seed=0)
+
+# Marker genes from
+# https://github.com/sta426hs2020/material/blob/8c57e3b/week13-07dec2020/workflow.Rmd#L152
+markers = list(map(str.upper, from_iterable(dict(
+    neuronal=["Snap25", "Stmn2", "Syn1", "Rbfox3", "Dlg4"],
+    neuronal_excitatory=["Slc17a7", "Camk2a", "Grin2b"],
+    neuronal_inhibitory=["Gad1", "Lhx6"],
+    astrocytes=["Aqp4", "Gfap", "Fgfr3", "Dio2"],
+    endothelial=["Cldn5", "Nostrin", "Flt1"],
+    microglia=["C1qb", "Tyrobp", "P2ry12", "Csf1r", "Irf8"],
+    oligodendrocyte=["Opalin", "Plp1", "Mag", "Mog"],
+    OPC=["Pdgfra", "Sox6", "Bcan"],
+).values())))
+
+out_dir = mkdir(Path(__file__).with_suffix(''))
 
 data_root = (Path(__file__).parent.parent.parent.parent / "data")
 assert data_root.is_dir()
@@ -31,29 +48,35 @@ assert fgcz_data["Feature ID"].is_unique
 
 fgcz_data = fgcz_data.drop(columns="Feature ID").set_index("gene_name")
 
-df = fgcz_data.transform(lambda x: np.log(1 + x))
+df = fgcz_data  #.transform(lambda x: np.log(1 + x))
 df = df / df.sum(axis=0)
-
-# genes = ["C9orf72", "PRPH", "UNC13A", "SLC1A2", "SNCG", "NEFH", "SOD2", "APOE", "LOX", "RNASE2"]
-# df = df[df.index.isin(genes)]
 
 from sklearn.manifold import TSNE
 X = pd.DataFrame(
     index=fgcz_meta.index,
     columns=['x', 'y'],
-    data=TSNE().fit_transform(df.T),
+    data=TSNE(random_state=rs).fit_transform(df[df.index.isin(markers)].T),
 )
 
-with Plox() as px:
-    for (key, df) in X.groupby(fgcz_meta.Condition):
-        px.a.plot(df.x, df.y, '.', label=key)
-        px.a.legend()
-    px.show()
+# Round the age column
+fgcz_meta.Age = fgcz_meta.Age.apply(lambda x: F"{np.round(x, -1)}s")
+fgcz_meta = fgcz_meta.sort_values(by='Age')
 
-for col in ['Gender', 'Age', 'pmDelay', 'LibConc_100_800bp', 'RIN']:
-    with Plox() as px:
+style = {'savefig.pad_inches': 0.01}
+
+for col in ['Condition', 'Gender', 'Age', 'Source']:
+    with Plox(style) as px:
+        markerstyles = ['^', 'v', 'p', 's', 'o']
+        for ((key, df), m) in zip(X.groupby(fgcz_meta[col]), markerstyles):
+            px.a.plot(df.x, df.y, ls='none', label=key, marker=m)
+            px.a.legend()
+        px.a.axis('off')
+        px.f.savefig(out_dir / F"tsne_by_{col}.png")
+
+for col in ['Gender', 'Age', 'pmDelay', 'LibConc_100_800bp', 'RIN', 'Source']:
+    with Plox(style) as px:
         data = OrderedDict(sorted(fgcz_meta[col].groupby(fgcz_meta.Condition)))
         px.a.hist(data.values(), label=list(data.keys()))
         px.a.set_xlabel(col)
         px.a.legend()
-        px.show()
+        px.f.savefig(out_dir / F"hist_by_{col}.png")
