e88d21577468af24e844bebbcb8c3fc6c4d3b836
RA <numpde@null.net>
2021-01-08 18:07:59 +0100

    mass commit

--- code/sandbox_ra/20201206-NNLS_Darmanis/datasource_r.py
+++ code/sandbox_ra/20201206-NNLS_Darmanis/datasource_r.py
@@ -1,15 +1,27 @@
 # RA, 2020-12-22
+# RA, 2021-01-08
+
+"""
+Load deconvolution results of the main R script.
+"""
 
 import twig
 import datasource
 
-# Attempt to read the deconvolution results by Bisque
 try:
-    fgcz_deco_by_bisque = datasource.read("code/**/20201222-Bisque/b_*/fgcz_darm_data.csv")
+    fgcz_deco_by_bisque = datasource.read("code/**/20201226-AllDeco/output/fgcz_darm_bisque_data.csv")
     fgcz_deco_by_bisque.columns = fgcz_deco_by_bisque.columns.str.replace('.', '-')
 except:
     twig.log.warning("Deconvolution results by Bisque not found.")
     fgcz_deco_by_bisque = None
 
+try:
+    fgcz_deco_by_music = datasource.read("code/**/20201226-AllDeco/output/fgcz_darm_music_data.csv")
+    fgcz_deco_by_music.columns = fgcz_deco_by_music.columns.str.replace('.', '-')
+except:
+    twig.log.warning("Deconvolution results by MuSiC not found.")
+    fgcz_deco_by_music = None
+
 if __name__ == '__main__':
     print(fgcz_deco_by_bisque)
+    print(fgcz_deco_by_music)
